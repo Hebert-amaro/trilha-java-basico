6010fae013d7b36a07cef42eb7af499bd45140a3
Hebert-amaro <hebert.amaro.araujo.quirino@ccc.ufcg.edu.br>
2025-07-01 21:10:45 -0300

    Terminado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        String numeroConta = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Construindo a mensagem com concat()
+        String mensagem = "Olá ".concat(nomeCliente)
+            .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+            .concat(agencia)
+            .concat(", conta ")
+            .concat(numeroConta)
+            .concat(" e seu saldo ")
+            .concat(String.valueOf(saldo))
+            .concat(" já está disponível para saque.");
+
+        System.out.println("\n" + mensagem);
+
+        scanner.close();
     }
 }
+
